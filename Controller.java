2988bb0ad113c0f72675a1ea5fa14684ff241285
GnomGrom1997 <94432870+GnomGrom1997@users.noreply.github.com>
2021-11-19 13:50:23 +0300

    cоздание приложение на java fx

---
+++ Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Button buttonreg;
+    public Label Message;
+
+    public void TreatMessage() {
+        Message.setText("Hello World");
+    }
+}
